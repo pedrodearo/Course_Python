11e2c4b76472709f573ede5c1e257d911e948068
Pedro Dearo Costa <contato.pedrodearo@gmail.com>
2024-08-28 21:50:27 -0300

    third commit

---
+++ Lógica de Programação/lesson_7.py
@@ -0,0 +1,10 @@
+""" 
+Variáveis são usadas para salvar algo na memória do computador.]
+PEP8: Inicie variáveis com letras minúsculas, pode usar números e underline.
+O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a uma variável
+Uso: nome_variavel = expressão
+"""
+complete_name = 'Pedro Dearo Costa'
+age = 18
+
+print(complete_name, age)
