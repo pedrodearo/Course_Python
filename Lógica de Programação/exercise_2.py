91893bed6de4ffd87c3529626833d8654060615a
Pedro Dearo <contato.pedrodearo@gmail.com>
2024-08-30 13:21:47 -0300

    fifth commit

---
+++ Lógica de Programação/exercise_2.py
@@ -0,0 +1,8 @@
+""" CALCULO DE IMC """
+
+name = 'Pedro Dearo'
+heigth = 1.8
+weigth = 73
+bmi = weigth / (heigth ** 2)
+
+print('Name:', name, '\n', 'Your BMI is:', bmi)
---
+++ Lógica de Programação/lesson_9.py
@@ -0,0 +1,10 @@
+""" CONCATENAÇÃO E REPETIÇÃO """
+
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+
+tres_vezes_pedro = 3 * '| Pedro |'
+print(tres_vezes_pedro)
